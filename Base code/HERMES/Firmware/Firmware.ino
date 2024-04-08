e938409a7f5410b48ad50d5a406146fe0d2ed8e5
GP <gurpreetsingh0204@gmail.com>
2024-04-07 18:38:36 -0800

    add serial comms

--- Base code/HERMES/Firmware/Firmware.ino
+++ Base code/HERMES/Firmware/Firmware.ino
@@ -1,36 +1,43 @@
 #include <Tic.h>
 
-TicI2C panMotor(14);  // Pan motor
-TicI2C tiltMotor(15);  // Tilt motor
-
+TicI2C panMotor(14);
+TicI2C tiltMotor(15); 
 float TargetAnglePan = 0;
 float TargetAngleTilt = 0;
-
 const float StepConstant = 127.2889;
 
 void setup() {
   panMotor.exitSafeStart();
   tiltMotor.exitSafeStart();
+  Serial.begin(9600);  // Initialize serial communication
 }
 
 void loop() {
-  // Retrieve current position of motors
-  int currentPanPosition = panMotor.getCurrentPosition();
-  int currentTiltPosition = tiltMotor.getCurrentPosition();
-
-  // Convert target angles to target positions
-  int targetPositionPan = -StepConstant * TargetAnglePan;
-  int targetPositionTilt = StepConstant * TargetAngleTilt;
-
-  // Set target positions for the motors
-  panMotor.setTargetPosition(targetPositionPan);
-  tiltMotor.setTargetPosition(targetPositionTilt);
-
-  // Wait until motors reach the desired positions
-  while (!isMotorAtPosition(panMotor) || !isMotorAtPosition(tiltMotor)) {  }
+  // Check if serial data is available
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    
+    int commaIndex = input.indexOf(',');
+    if (commaIndex != -1) {
+      //extract angle
+      TargetAnglePan = input.substring(0, commaIndex).toFloat();
+      TargetAngleTilt = input.substring(commaIndex + 1).toFloat();
+      
+      //calculate position
+      int targetPositionPan = -StepConstant * TargetAnglePan;
+      int targetPositionTilt = StepConstant * TargetAngleTilt;
+      
+      // give comand
+      panMotor.setTargetPosition(targetPositionPan);
+      tiltMotor.setTargetPosition(targetPositionTilt);
+      
+      // Wait until command is executed
+      while (!isMotorAtPosition(panMotor) || !isMotorAtPosition(tiltMotor)) {}
+    }
+  }
 }
 
-//helper method
+// Helper method
 bool isMotorAtPosition(TicI2C& motor) {
   int targetPosition = motor.getTargetPosition();
   int currentPosition = motor.getCurrentPosition();
